fdff7b99a8aa30bea8a0e93c6dfc5c9fa77a2c55
qwqdanchun <287182701@qq.com>
2021-05-08 00:47:13 +0800

    Update ArtifactBuild.cmd

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "DcMacro\bin\Release" -ArchiveFileName "DcMacro.zip" -Format Zip
+powershell Compress-7Zip "Macro\bin\Release" -ArchiveFileName "DcMacro.zip" -Format Zip
 :exit
 popd
 @echo on
